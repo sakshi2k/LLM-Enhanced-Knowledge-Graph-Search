f569e448b717f9f7f6cb1e6048e2d5b2958516ec
sakshiAtArticly <sakshi@articly.de>
2026-01-11 17:57:23 +0100

    sample payloads used for dev purpose added

---
+++ client/sample_input_jsons_for_dev/sample_data_payload.py
@@ -0,0 +1,123 @@
+SAMPLE_DATA = {
+   "query":"What changed in European Central Bank messaging on growth between 2023 and 2025",
+   "filters":{
+      "speaker":"None",
+      "from_date":"2023-01-01",
+      "to_date":"2023-12-31"
+   },
+   "results":[
+      {
+         "speech_id":"SPE000252",
+         "title":"The path ahead",
+         "speaker":"Christine Lagarde",
+         "date":"2023-03-22",
+         "topics":[
+            "Measures of underlying inflation tracked by the EUROPEAN CENTRAL BANK currently range between 4 and 8"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000252-TOP000738-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=The+path+ahead&year=2023&boardmember=Christine+Lagarde"
+      },
+      {
+         "speech_id":"SPE000249",
+         "title":"Urgent and vitally important: 2023 as a key milestone in stepping up the management of climate and environmental risks",
+         "speaker":"Frank Elderson",
+         "date":"2023-03-27",
+         "topics":[
+            "Urgent and vitally important 2023 as a key milestone in stepping up the management of climate and environmental risks Keynote speech by Frank Elderson Member of the Executive Board of the EUROPEAN CENTRAL BANK and Vice Chair of the Supervisory Board of the EUROPEAN CENTRAL BANK at the Foreign Bankers Association FBA 30th anniversary Urgent and vitally important 2023 as a key milestone in stepping up the management of climate and environmental risks Keynote speech by Frank Elderson Member of the Executive Board of the EUROPEAN CENTRAL BANK and Vice Chair of the Supervisory Board of the EUROPEAN CENTRAL BANK at the Foreign Bankers Association FBA 30th anniversary Amsterdam 27 March 2023 Thank you for inviting me to the FBA s 30th anniversary event"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000249-TOP000730-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Urgent+and+vitally+important%3A+2023+as+a+key+milestone+in+stepping+up+the+management+of+climate+and+environmental+risks&year=2023&boardmember=Frank+Elderson"
+      },
+      {
+         "speech_id":"SPE000223",
+         "title":"Central clearing in turbulent times: frontiers in regulation and oversight",
+         "speaker":"Fabio Panetta",
+         "date":"2023-06-22",
+         "topics":[
+            "Central clearing in turbulent times frontiers in regulation and oversight Keynote speech by Fabio Panetta Member of the Executive Board of the EUROPEAN CENTRAL BANK at the Fifth Joint Deutsche Bundesbank European Central Bank and Federal Reserve Bank of Chicago Conference on CCP Risk Management Central clearing in turbulent times frontiers in regulation and oversight Keynote speech by Fabio Panetta Member of the Executive Board of the EUROPEAN CENTRAL BANK at the Fifth Joint Deutsche Bundesbank European Central Bank and Federal Reserve Bank of Chicago Conference on CCP Risk Management Frankfurt am Main 22 June 2023"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000223-TOP000648-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Central+clearing+in+turbulent+times%3A+frontiers+in+regulation+and+oversight&year=2023&boardmember=Fabio+Panetta"
+      },
+      {
+         "speech_id":"SPE000201",
+         "title":"Welcome address",
+         "speaker":"Isabel Schnabel",
+         "date":"2023-10-04",
+         "topics":[
+            "Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Frankfurt am Main 4 October 2023"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000201-TOP000585-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Welcome+address&year=2023&boardmember=Isabel+Schnabel"
+      },
+      {
+         "speech_id":"SPE000201",
+         "title":"Welcome address",
+         "speaker":"Mario Draghi",
+         "date":"2023-10-04",
+         "topics":[
+            "Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Frankfurt am Main 4 October 2023"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000201-TOP000585-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Welcome+address&year=2023&boardmember=Mario+Draghi"
+      },
+      {
+         "speech_id":"SPE000201",
+         "title":"Welcome address",
+         "speaker":"Christine Lagarde",
+         "date":"2023-10-04",
+         "topics":[
+            "Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Frankfurt am Main 4 October 2023"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000201-TOP000585-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Welcome+address&year=2023&boardmember=Christine+Lagarde"
+      },
+      {
+         "speech_id":"SPE000201",
+         "title":"Welcome address",
+         "speaker":"Philip R. Lane",
+         "date":"2023-10-04",
+         "topics":[
+            "Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Frankfurt am Main 4 October 2023"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000201-TOP000585-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Welcome+address&year=2023&boardmember=Philip+R.+Lane"
+      },
+      {
+         "speech_id":"SPE000212",
+         "title":"Communication and monetary policy",
+         "speaker":"Christine Lagarde",
+         "date":"2023-09-04",
+         "topics":[
+            "Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Welcome address Welcome address by Christine Lagarde President of the EUROPEAN CENTRAL BANK at the EUROPEAN CENTRAL BANK Conference on Monetary Policy bridging science and practice Frankfurt am Main 4 October 2023"
+         ],
+         "confidence":1.0,
+         "provenance":[
+            "SPE000212-TOP000585-covers"
+         ],
+         "url":"https://www.ecb.europa.eu/press/pubbydate/html/index.en.html?search_term=Communication+and+monetary+policy&year=2023&boardmember=Christine+Lagarde"
+      }
+   ],
+   "verified":True
+}
